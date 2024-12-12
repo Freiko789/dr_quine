acc57959b67a69c07ecbe72c6194419ce415ff91
wsl <davidtassel04@gmail.com>
2024-12-12 10:55:51 +0100

    fix com

--- Colleen.c
+++ Colleen.c
@@ -4,10 +4,14 @@ int	Somme(int a, int b) {
 	a += b;
 	return a;
 }
-//premier commentaire
+/*
+premier commentaire
+*/
 int main() {
-	char *program = "#include <stdio.h>%c%cint	Somme(int a, int b) {%c	a += b;%c	return a;%c}%c//premier commentaire%cint main() {%c	char *program = %c%s%c;%c	Somme(1,1);%c	/*2 eme commentaire*/%c	printf(program, 10, 10, 10, 10, 10, 10, 10, 10, 34, program, 34, 10, 10, 10, 10, 10);%c}%c";
+	char *program = "#include <stdio.h>%c%cint	Somme(int a, int b) {%c	a += b;%c	return a;%c}%c/*%cpremier commentaire%c*/%cint main() {%c	char *program = %c%s%c;%c	Somme(1,1);%c	/*%c		2 eme commentaire%c	*/%c	printf(program, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, program, 34, 10, 10, 10, 10, 10, 10, 10);%c}%c";
 	Somme(1,1);
-	/*2 eme commentaire*/
-	printf(program, 10, 10, 10, 10, 10, 10, 10, 10, 34, program, 34, 10, 10, 10, 10, 10);
+	/*
+		2 eme commentaire
+	*/
+	printf(program, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, program, 34, 10, 10, 10, 10, 10, 10, 10);
 }
